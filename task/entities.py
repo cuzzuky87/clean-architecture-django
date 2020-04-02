0bf4a0d34348cbc5f7ee55043b6bb13d1ae006f6
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-04-02 23:32:48 +0900

    entity実装

--- task/entities.py
+++ task/entities.py
@@ -1,10 +1,19 @@
 from django.utils import timezone
+import datetime
+from typing import Final
+
+class DomainException(Exception):
+    pass
 
 class TaskStatus(Enum):
     Undone = 0
     Done = 1
 
+
 class Task:
+    
+    __MAX_POSTPONE_COUNT:Final = 3
+
     def __init__(
         self,
         name,
@@ -19,6 +28,17 @@ class Task:
         self.task_status = task_status
         self.postpone_count = postpone_count
         self.due_date = due_date
+
+    def postpone(self):
+        validate_postpone_count()
+        self.due_date = self.due_date + datetime.timedelta(days=1)
+        self.postpone_count += 1
+    
+    def validate_postpone_count(self):
+        if self.postpone_count >= __MAX_POSTPONE_COUNT:
+            raise DomainException("最大延期回数を超えています")
+        
+
         
 
 
