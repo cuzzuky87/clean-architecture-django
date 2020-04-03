e9eb8d54b36d424fd6c7c2d9df7a27298193f391
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-04-03 23:16:41 +0900

    imp orm task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    name = models.CharField(max_length=150)
+    due_date = models.DateField()
+    TASK_STATUS = [
+        (0,"UNDONE"),
+        (1,"DONE")
+    ]
+    task_status = models.IntegerField(choices=TASK_STATUS)
+    postpone_count = models.IntegerField()
