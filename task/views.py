3d7d3453adb7da62c45aefb540ff78d7538d6c83
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-04-09 23:45:59 +0900

    wip usecase

--- task/views.py
+++ task/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_503_SERVICE_UNAVAILABLE
+from rest_framework.exceptions import NotFound, ValidationError, APIException
+from .entities import DomainException
+from .use_case import TaskUseCase
 
-# Create your views here.
+
+def postpone_task(request, id):
+    # validate
+    # call use case
+    # handleException
+    # return serialized data
+    pass
